74f37d2d1c4657064d60ff7a6eafcc5a331ff66f
bulbulahmed9 <bulbulahmed.tk@gmail.com>
2020-02-06 21:08:53 +0600

    build folder added

---
+++ build/precache-manifest.89d90355597660a4e49db8696271f716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3999fdff17efa591edb55c70d9e81385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ce43f023e433a25e39",
+    "url": "/static/css/main.ed3fa02d.chunk.css"
+  },
+  {
+    "revision": "cbab9914caa6b7d69145",
+    "url": "/static/js/2.26d52178.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/static/js/2.26d52178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ce43f023e433a25e39",
+    "url": "/static/js/main.7da8480d.chunk.js"
+  },
+  {
+    "revision": "6a6959ea3f08cddd6da7",
+    "url": "/static/js/runtime-main.41c3d2fd.js"
+  }
+]);
